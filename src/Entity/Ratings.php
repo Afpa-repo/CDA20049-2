5d1edc7353735ad50223212e81522bdf174522cf
Mathieu <mathieuantoine4@gmail.com>
2020-10-26 16:15:47 +0100

    initialised db

---
+++ src/Entity/Ratings.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RatingsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RatingsRepository::class)
+ */
+class Ratings
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $rating;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateRating;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getDateRating(): ?\DateTimeInterface
+    {
+        return $this->dateRating;
+    }
+
+    public function setDateRating(\DateTimeInterface $dateRating): self
+    {
+        $this->dateRating = $dateRating;
+
+        return $this;
+    }
+}
