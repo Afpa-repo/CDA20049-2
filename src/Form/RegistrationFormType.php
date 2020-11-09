27eb5bc1f82611f8936ca660557e08ccfcf4efda
Isabelle Bengrine <ibengrine@yahoo.com>
2020-11-09 15:35:06 +0100

    Modified registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,20 +5,32 @@ namespace App\Form;
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use FOS\CKEditorBundle\Builder\JsonBuilder;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail address',
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your e-mail address']),
+                    new Email(['message' => 'This'.'{{ value }}'.'is not a valid e-mail address.']),
+                    new Regex(['pattern' => '/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/',
+                        'message' => 'Check your entry: invalid character']),
+                ],
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -36,9 +48,27 @@ class RegistrationFormType extends AbstractType
                 ],
 
             ])
-            ->add('firstname')
-            ->add('lastname')
+            ->add('firstname', TextType::class, [
+                'label' => 'First Name',
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your first name']),
+                    new Length(['min' => 2]),
+                    new Regex(['pattern' => '/^[A-Za-zéèàçâêûîôäëüïö\_\-\s]+$/',
+                        'message' => 'Check your entry: invalid character']),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Last Name',
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your last name']),
+                    new Length(['min' => 2]),
+                    new Regex(['pattern' => '/^[A-Za-zéèàçâêûîôäëüïö\_\-\s]+$/',
+                        'message' => 'Check your entry: invalid character']),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label'    => 'Please agree to our terms and conditions',
+                'required' => true,
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
