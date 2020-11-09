19e261c87876b85ea32b437fe98e3f533cd3dd47
Guillaume GALZY <guillaume.galzy@hotmail.fr>
2020-11-09 09:04:02 +0100

    Ajout méthode to__string à l'entité IngredientCategory

--- src/Entity/IngredientCategory.php
+++ src/Entity/IngredientCategory.php
@@ -34,6 +34,11 @@ class IngredientCategory
         $this->RelatedIngredients = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
